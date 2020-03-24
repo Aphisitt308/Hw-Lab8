7c9af9a51957c02f48f216833a9fcb260b9db7d2
Aphisitt308 <bam52170@gmail.com>
2020-03-24 18:09:49 +0700

    Updates

---
+++ precache-manifest.a49e44a77cd11cc9356d7c5d0cd5cb02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7607258e959eb121aa75324866980f26",
+    "url": "/Hw-Lab8/index.html"
+  },
+  {
+    "revision": "116c41f9a31d86b5b708",
+    "url": "/Hw-Lab8/static/css/main.4851e9fc.chunk.css"
+  },
+  {
+    "revision": "ed3c9f1685dae72667ee",
+    "url": "/Hw-Lab8/static/js/2.ce8f0f81.chunk.js"
+  },
+  {
+    "revision": "9cc59aa88bbb965336535eb6b360773d",
+    "url": "/Hw-Lab8/static/js/2.ce8f0f81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "116c41f9a31d86b5b708",
+    "url": "/Hw-Lab8/static/js/main.c10c1e23.chunk.js"
+  },
+  {
+    "revision": "d67d178a7da26180c278",
+    "url": "/Hw-Lab8/static/js/runtime-main.708a54e2.js"
+  }
+]);
